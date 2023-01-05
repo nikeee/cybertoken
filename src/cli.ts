62a897ce03bc879788e6fa898a738271b933b3a1
Niklas Mollenhauer <nikeee@outlook.com>
2023-01-05 03:06:35 +0100

    Fix CD

--- src/cli.ts
+++ src/cli.ts
@@ -1,15 +1,18 @@
 import { createTokenGenerator } from "./index.js";
 
-const prefixWithoutUnderscore = process.argv[2] ?? process.env.CYBERTOKEN_PREFIX;
+const prefixWithoutUnderscore =
+  process.argv[2] ?? process.env.CYBERTOKEN_PREFIX;
 
-if(!prefixWithoutUnderscore) {
+if (!prefixWithoutUnderscore) {
   console.error("Please specify a prefix for the token.");
   console.error("A prefix must be a non-empty string.");
   console.error();
   console.error("Usage:");
   console.error("  cybertoken <prefix>");
   console.error();
-  console.error("You can also provide the prefix via the CYBERTOKEN_PREFIX environment variable.");
+  console.error(
+    "You can also provide the prefix via the CYBERTOKEN_PREFIX environment variable."
+  );
   console.error("For example:");
   console.error("  CYBERTOKEN_PREFIX=foo cybertoken");
   process.exit(1);
@@ -21,4 +24,3 @@ const tokenGenerator = createTokenGenerator({
 
 const token = tokenGenerator.generateToken();
 console.log(token);
-
